f8ea0d1712325af79132e5d59def0b821c82be49
Uzochukwu Okafor <uzochukwuokafor01@gmail.com>
2024-11-18 13:38:02 +0100

    feat: Add the page load animation using GSAP

--- script.js
+++ script.js
@@ -1,13 +1,42 @@
 "use strict";
 
 gsap.registerPlugin(CustomEase);
-
-function initializeAnimation() {}
-
-window.addEventListener("DOMContentLoaded", initializeAnimation);
+CustomEase.create("ease-in-out-quart", "0.77,0,0.175,1");
 
 document.addEventListener("keydown", (event) => {
   if (event.shiftKey && event.key.toLowerCase() === "g") {
-    document.querySelector(".overlay").classList.toggle("active");
+    document.querySelector(".c-grid_overlay").classList.toggle("cc-is-visible");
   }
 });
+
+const initAnimation = () => {
+  const loaderTl = gsap.timeline({ delay: 1 });
+
+  loaderTl
+    .to("[data-loader-text]", {
+      y: 0,
+      stagger: 0.17,
+      ease: "power4.out",
+      duration: 0.55,
+    })
+    .to(
+      "[data-loader-text]",
+      {
+        y: -100,
+        stagger: 0.17,
+        ease: "power4.in",
+        duration: 0.65,
+      },
+      "+=0.575"
+    )
+    .to(".c-grid_block", {
+      scaleX: "0",
+      transformOrigin: "left",
+      ease: "ease-in-out-quart",
+      stagger: 0.02,
+      duration: 0.725,
+      onComplete: () => gsap.set(".c-loader", { pointerEvents: "none" }),
+    });
+};
+
+document.addEventListener("DOMContentLoaded", initAnimation);
